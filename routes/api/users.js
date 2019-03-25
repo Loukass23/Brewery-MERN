384608fa9ff7acd8e4e80e77a610d44fad822f49
Marcus  Martinez <gmarcusmartinez@gmail.com>
2019-03-25 14:37:09 +0100

    Login functionality and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
+//Import Validation
+const validateLoginInput = require('../../validation/login');
+const validateRegisterInput = require('../../validation/register');
+
+const router = express.Router();
 /*  POST api/users/register
     desc: Register a new user
     access: Public
  */
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   /*First we use mongoose method 'findOne' to 
   make sure an account with email passed in 
   does not already exist */
@@ -43,4 +55,53 @@ router.post('/register', (req, res) => {
       }
     });
 });
+/*  POST api/users/login
+    desc: Return a Jason Web Token
+    access: Public
+ */
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+    //Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //Create a jwt payload with user information
+        const payload = { id: user.id, name: user.name, img: user.img };
+        //Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 28880 },
+          (err, token) => {
+            res.json({ success: true, token: 'Bearer ' + token });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: 'Password is incorrect' });
+      }
+    });
+  });
+});
+/*  POST api/users/current
+    desc: Return current user
+    access: Private
+ */
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json(req.user);
+  }
+);
 module.exports = router;
